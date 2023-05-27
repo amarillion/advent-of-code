9b6523f4c12f732664f24971ab4df1df3414546e
Martijn van Iersel <mvaniersel@gmail.com>
2023-05-27 16:52:11 +0200

    2022, Day 19 part 1

--- 2022/day19/solution.cpp
+++ 2022/day19/solution.cpp
@@ -12,56 +12,13 @@
 
 using namespace std;
 
-enum Res { ORE, CLAY, OBSIDIAN, GEODE, ORE_ROBOT, CLAY_ROBOT, OBSIDIAN_ROBOT, GEODE_ROBOT };
-using Recipe = map<Res, int>;
+enum Res { ORE, CLAY, OBSIDIAN, GEODE, ORE_ROBOT, CLAY_ROBOT, OBSIDIAN_ROBOT, GEODE_ROBOT, SKIP };
 
+using Recipe = map<Res, int>;
 using Resources = array<short, 8>; // this is the Node type
 using Option = Res;
-
-struct State {
-	Resources prev { 0, 0, 0, 0, 1, 0, 0, 0 };
-	Resources resources { 0, 0, 0, 0, 1, 0, 0, 0 };
-	short minute = 1;
-//	string history;
-
-	bool operator==(const State &other) const noexcept {
-		return prev == other.prev && resources == other.resources && minute == other.minute;
-	}
-
-	// custom hash function
-	size_t operator()(const State& state) const noexcept {
-//		size_t hash = 5 * resources[0] + 7 * resources[1] + 11 * resources[2] + 13 * resources[3] + 17 * resources[4] +
-//			   19 * resources[5]
-//			   + 23 * resources[6] + 29 * resources[7] +
-//			   31 * prev[0] + 37 * prev[1] + 39 * prev[2] + 41 * prev[3] + 43 * prev[4] + 47 * prev[5] +
-//			   51 * prev[6] + 53 * prev[7] +
-//			   57 * minute;
-		std::hash<short> hasher;
-		size_t result = 0;
-		for(size_t i = 0; i < 8; ++i) {
-			result = result * 31 + hasher(state.resources[i]); // ??
-			result = result * 31 + hasher(state.prev[i]); // ??
-		}
-		result = result + 31 * hasher(state.minute);
-		return result;
-	};
-};
-
+using State = Resources;
 using Blueprint = map<Res, Recipe>;
-
-ostream &operator<<(ostream &os, const Res &res) {
-	switch (res) {
-		case Res::ORE: os << "ore"; break;
-		case Res::CLAY: os << "clay"; break;
-		case Res::OBSIDIAN: os << "obs"; break;
-		case Res::GEODE: os << "geode"; break;
-		case Res::ORE_ROBOT: os << "R.ORE"; break;
-		case Res::CLAY_ROBOT: os << "R.CLAY"; break;
-		case Res::OBSIDIAN_ROBOT: os << "R.OBS"; break;
-		case Res::GEODE_ROBOT: os << "R.GEODE"; break;
-	}
-	return os;
-};
 
 const char *toString(const Res &res) {
 	switch (res) {
@@ -73,6 +30,7 @@ const char *toString(const Res &res) {
 		case Res::CLAY_ROBOT: return "R.CLAY";
 		case Res::OBSIDIAN_ROBOT: return "R.OBS";
 		case Res::GEODE_ROBOT: return "R.GEODE";
+		case Res::SKIP: return "skip";
 	}
 };
 
@@ -115,192 +73,86 @@ bool applyIfPossible(const Recipe &r, Resources &resources) {
 	return false;
 }
 
-/*
-int countProduct(const Recipe &rp, const Resources &resources) {
-	int sum = 0;
-	for (const auto &[k, v]: rp) {
-		if (v > 0) {
-			if (resources.contains(k)) {
-				sum += resources.at(k) * v;
-			}
-		}
-	}
-	return sum;
-}
-
-void sortByProduct(Blueprint &bp, const Resources &resources) {
-	sort(bp.begin(), bp.end(), [&](const Recipe &a, const Recipe &b){
-		return countProduct(b, resources) - countProduct(a, resources);
-	});
-}
-*/
-
 vector<Option> getOptions(const Blueprint &bp, const State &state) {
-
-//	if (state.minute < 5)  {
-//		return vector<Option> { Res::ORE_ROBOT };
-//	}
-//	else if (state.minute < 8) {
-//		return vector<Option> { Res::CLAY_ROBOT };
-//	}
-//	else if (state.minute < 10) {
-//		return vector<Option> { Res::ORE_ROBOT };
-//	}
-//	else if (state.minute < 15) {
-//		return vector<Option> { Res::CLAY_ROBOT, Res::OBSIDIAN_ROBOT };
-//	}
-//	else if (state.minute < 19) {
-//		return vector<Option> { Res::CLAY_ROBOT, Res::OBSIDIAN_ROBOT };
-//	}
-//	else {
-//		return vector<Option> { Res::OBSIDIAN_ROBOT, Res::GEODE_ROBOT };
-//	}
-
-	const Resources &resources = state.resources;
-	const Resources &prev = state.prev;
 	vector<Option> result;
-	// it seems to be an extra restriction that we can't build two of the same robot in the same minute.
-	if(resources[Res::ORE] < 12) {
-		result.push_back(Res::ORE_ROBOT);
-	}
-	if (resources[Res::CLAY] < 28) {
-		result.push_back(Res::CLAY_ROBOT);
-	}
-	if (resources[Res::CLAY_ROBOT] > 0) {
-		result.push_back(Res::OBSIDIAN_ROBOT);
-	}
-	if (resources[Res::OBSIDIAN_ROBOT] > 0) {
+	bool canGeode = canApply(bp.at(Res::GEODE_ROBOT), state);
+	bool canObs = canApply(bp.at(Res::OBSIDIAN_ROBOT), state);
+	bool canOre = canApply(bp.at(Res::ORE_ROBOT), state);
+	bool canClay = canApply(bp.at(Res::CLAY_ROBOT), state);
+
+	// smart culling...
+	if (canGeode) {
 		result.push_back(Res::GEODE_ROBOT);
 	}
+	if (canObs) {
+		result.push_back(Res::OBSIDIAN_ROBOT);
+	}
+	if (!canGeode && !canObs) {
+		if (canOre && state[Res::ORE_ROBOT] < 10) {
+			result.push_back(Res::ORE_ROBOT);
+		}
+		if (canClay && state[Res::CLAY_ROBOT] < 10) {
+			result.push_back(Res::CLAY_ROBOT);
+		}
+	}
+	// if we can't do both, we may want to hold off...
+	if (!(canClay && canOre)) {
+		result.push_back(Res::SKIP);
+	}
+
 	return result;
 };
 
-const int MAX_MINUTES = 24;
-
-int score(const State &s) {
-	int minutesLeft = MAX_MINUTES - s.minute;
-//	return
-//			s.resources[GEODE] * 100 +
-//			s.resources[GEODE_ROBOT] * minutesLeft * 16 +
-//			s.resources[OBSIDIAN] * 1 +
-//			s.resources[OBSIDIAN_ROBOT] * minutesLeft * 8 +
-//			s.resources[CLAY] * 1 +
-//			s.resources[CLAY_ROBOT] * minutesLeft * 4 +
-//			s.resources[ORE] * 1 +
-//			s.resources[ORE_ROBOT] * minutesLeft * 2;
-	return
-			s.resources[GEODE] * 100 +
-			s.resources[GEODE_ROBOT] * 16 +
-//			s.resources[OBSIDIAN] * 1 +
-			s.resources[OBSIDIAN_ROBOT] * 8 +
-//			s.resources[CLAY] * 1 +
-			s.resources[CLAY_ROBOT] * 4 +
-//			s.resources[ORE] * 1 +
-			s.resources[ORE_ROBOT] * 2;
-}
-
-void advanceMinute(State &result) {
-	// mining takes place after crafting, but with old robot counts.
-	result.resources[Res::ORE] += result.prev[Res::ORE_ROBOT];
-	result.resources[Res::CLAY] += result.prev[Res::CLAY_ROBOT];
-	result.resources[Res::OBSIDIAN] += result.prev[Res::OBSIDIAN_ROBOT];
-	result.resources[Res::GEODE] += result.prev[Res::GEODE_ROBOT];
-//		cout << "Minute: " << result.minute << ", " <<
-//			result.resources[Res::ORE] << " Ore, " <<
-//			result.resources[Res::CLAY] << " Clay, " <<
-//			result.resources[Res::OBSIDIAN] << " Obsidian, and " <<
-//			result.resources[Res::GEODE] << " Geodes. Score: " << score(state) << endl;
-	result.minute++;
-//	result.history += string_format("Minute %i\n", result.minute);
-	result.prev = result.resources;
-}
-
 State applyOption(const Blueprint &bp, const State &state, const Option &opt) {
-	// option is: next I'll buy X, waiting as many minutes as necessary...
-	State result = state; // make copy
+	State next = state; // make copy
 
-//	cout << "Mining until we can buy " << opt << endl;
-	while (!canApply(bp.at(opt), result.resources)) {
-		advanceMinute(result);
-		if (result.minute > MAX_MINUTES) {
-			return result; // end reached.
-		}
+	if (opt != Res::SKIP) {
+		bool success = applyIfPossible(bp.at(opt), next);
+	//	result.history += string_format("Bought: %s\n", toString(opt));
+	//	cout << "After crafting an " << opt << " we have " << result.resources[opt] << endl;
+		assert(success);
 	}
 
-	bool success = applyIfPossible(bp.at(opt), result.resources);
-//	result.history += string_format("Bought: %s\n", toString(opt));
-//	cout << "After crafting an " << opt << " we have " << result.resources[opt] << endl;
-	assert(success);
+	next[Res::ORE] += state[Res::ORE_ROBOT];
+	next[Res::CLAY] += state[Res::CLAY_ROBOT];
+	next[Res::OBSIDIAN] += state[Res::OBSIDIAN_ROBOT];
+	next[Res::GEODE] += state[Res::GEODE_ROBOT];
 
-	advanceMinute(result);
-
-	return result;
-}
-
-bool greedyCmp(const State &a, const State &b) {
-	return score(a) < score(b);
+	return next;
 }
 
 int search(const Blueprint &bp) {
-	vector<State> open { {} };
-	int max = -1;
-	int maxIt = 100'000'000;
 	int it = 0;
-	unordered_set<State, State> visited { {} };
-	while(!open.empty()) {
+	State start { 0 };
+	start[Res::ORE_ROBOT] = 1;
+	vector<State> open { start };
+	vector<State> next;
+	for (int minute = 1; minute <= 24; ++minute) {
+		next.clear();
 
-		pop_heap(open.begin(), open.end(), greedyCmp);
-		State current = open.back();
-		open.pop_back();
+		while(!open.empty()) {
+			State current = open.back();
+			open.pop_back();
+			it++;
 
-		if (++it == maxIt) {
-			cout << "Max iterations reached " << endl;
-			return max; // NOTE - if there are lots of branches at the start, may not have set max at all...
-		}
-
-		if (current.minute >= MAX_MINUTES) {
-			int val = current.resources[GEODE];
-			if (val > max) {
-				max = val;
-//				cout << "new record: " << max << " at iteration " << it << endl;
-//				cout << current.history << "=============================\n";
-			}
-		}
-		else {
 			for (auto option: getOptions(bp, current)) {
 				State newState = applyOption(bp, current, option);
-
-				if (!visited.contains(newState)) {
-					open.push_back(newState);
-					push_heap(open.begin(), open.end(), greedyCmp);
-					visited.insert(newState);
-				}
+				next.push_back(newState);
 			}
 		}
+//		cout << "Minute " << minute << " Open set " << next.size() << endl;
+		open = next;
 	}
-//	cout << "Completed after " << it << " iterations." << endl;
-	return max;
-}
-
-int getMaxRecursively(const Blueprint &bp, const State &state) {
-	if (state.minute >= MAX_MINUTES) {
-		return state.resources[GEODE];
-	}
-	else {
-		// get max...
-		int max = 0;
-		for (auto option: getOptions(bp, state)) {
-			State newState = applyOption(bp, state, option);
-			int result = getMaxRecursively(bp, newState);
-			if (result > max) max = result;
+//	cout << "Total iterations: " << it << endl;
+	int max = -1;
+	for (const auto &state: open) {
+		int val = state[Res::GEODE];
+		if (val > max) {
+			max = val;
 		}
-		return max;
 	}
-}
 
-int simulate(const Blueprint &bp) {
-	State state {};
-	return getMaxRecursively(bp, state);
+	return max;
 }
 
 int solve1(const vector<Blueprint> &bps) {
@@ -313,53 +165,9 @@ int solve1(const vector<Blueprint> &bps) {
 	return sum;
 }
 
-/*
- * BIG misconception: you can only produce one robot per day, no matter which type?
- *
- * Maybe rewrite with this idea in mind?
- */
-
-/*
- *
- * Current best solution:
-Blueprint 1 0
-Blueprint 2 0
-Blueprint 3 0
-Blueprint 4 0
-Blueprint 5 0
-Blueprint 6 0
-Blueprint 7 0
-Blueprint 8 0
-Blueprint 9 9
-Blueprint 10 4
-Blueprint 11 3
-Blueprint 12 4
-Blueprint 13 3
-Blueprint 14 0
-Blueprint 15 0
-Blueprint 16 4
-Blueprint 17 8
-Blueprint 18 7
-Blueprint 19 3
-Blueprint 20 4
-Blueprint 21 0
-Blueprint 22 0
-Blueprint 23 15
-Blueprint 24 12
-Blueprint 25 6
-Blueprint 26 3
-Blueprint 27 2
-Blueprint 28 0
-Blueprint 29 0
-Blueprint 30 13
-2009
- But this answer is too low...
- */
 int main() {
 	auto testInput = readData("day19/test-input");
 	assert(solve1(testInput) == 33);
 	auto input = readData("day19/input");
-//	cout << input << endl;
 	cout << solve1(input);
-	// answer 2009 is too low
 }
