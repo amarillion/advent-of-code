3c8a1affad22d520faaa66002f94cf937b69680f
Martijn van Iersel <mvaniersel@gmail.com>
2023-05-30 13:30:06 +0200

    Extra debugging utilities

--- 2022/common/collectionutil.h
+++ 2022/common/collectionutil.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <set>
+#include <map>
+#include <array>
 
 /**
  * Print a vector<T> to a stream, enclosed by [] and delimited by ,
@@ -8,26 +10,44 @@
  */
 template<typename T>
 std::ostream &operator<< (std::ostream &os, const std::vector<T> &v) {
-	char sep = '[';
+	os << '[';
+	bool first = true;
 	for (const auto &item: v) {
-		os << sep << item;
-		sep = ',';
+		if (first) { first = false; } else { os <<  ','; }
+		os << item;
 	}
 	os << ']';
 	return os;
 }
 
 /**
- * Print a vector<T> to a stream, enclosed by [] and delimited by ,
+ * Print a set<T> to a stream, enclosed by [] and delimited by ,
  * T must have operator<< defined.
  */
 template<typename T>
 std::ostream &operator<< (std::ostream &os, const std::set<T> &v) {
-	char sep = '[';
+	os << '[';
+	bool first = true;
 	for (const auto &item: v) {
-		os << sep << item;
-		sep = ',';
+		if (first) { first = false; } else { os <<  ','; }
+		os << item;
 	}
 	os << ']';
+	return os;
+}
+
+/**
+ * Print a map<K, V> to a stream, as k => v and delimited by ,
+ * K and V must have operator<< defined.
+ */
+template<typename K, typename V>
+std::ostream &operator<< (std::ostream &os, const std::map<K, V> &map) {
+	bool first = true;
+	os << "{ ";
+	for (const auto &[k, v]: map) {
+		if (first) { first = false; } else { os <<  ", "; }
+		os << k << " => " << v;
+	}
+	os << " }";
 	return os;
 }
