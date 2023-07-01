f85da6916e68734b916e72f01037fa9b83d7aa0d
Martijn van Iersel <mvaniersel@gmail.com>
2023-07-01 13:06:56 +0200

    Refactor part 1 to prep for part 2

--- 2022/day22/solution.cpp
+++ 2022/day22/solution.cpp
@@ -7,8 +7,113 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex>
-#include <unordered_set>
+
+// part 2: 10:46-11:40;
+
+enum class Orientation {
+	EAST, SOUTH, WEST, NORTH
+};
+
+struct Face {
+	Point uv;
+	Face* neighbor[4];
+	int deltaOrientation[4];
+};
+
+struct Map {
+	int tileSize;
+	Face faces[6];
+};
+
+void link(Face *src, Face *dest, int orientation, bool reverse = true) {
+	src->neighbor[orientation] = dest;
+	link(dest, src, (orientation + 2) % 4, false);
+}
+
+Map initTest() {
+	enum {
+		A, B, C, D, E, F
+	};
+/*
+
+ 4x4
+
+  A
+BCD
+  EF
+
+ */
+	Map result;
+	result.tileSize = 4;
+	Face *faces = result.faces;
+	faces[A] = {
+			{ 8, 0, },
+			&faces[A], &faces[D], &faces[A], &faces[E], 0, 0, 0, 0
+	};
+	faces[B] = {
+			{ 0, 4, },
+			&faces[C], &faces[B], &faces[D], &faces[B], 0, 0, 0, 0
+	};
+	faces[C] = {
+			{ 4, 4 },
+			&faces[D], &faces[C], &faces[B], &faces[C], 0, 0, 0, 0
+	};
+	faces[D] = {
+			{8, 4},
+			&faces[B], &faces[E], &faces[C], &faces[A], 0, 0, 0, 0
+	};
+	faces[E] = {
+			{8, 8},
+			&faces[F], &faces[A], &faces[F], &faces[D], 0, 0, 0, 0
+	};
+	faces[F] = {
+			{12, 8},
+			&faces[E], &faces[F], &faces[E], &faces[F], 0, 0, 0, 0
+	};
+	return result;
+}
+
+Map initMain() {
+	enum { A, B, C, D, E, F };
+
+	Map result;
+	result.tileSize = 50;
+	Face *faces = result.faces;
+	/*
+50x50
+
+ AB
+ C
+DE
+F
+*/
+	faces[A] = {
+			{50, 0},
+			&faces[B], &faces[C], &faces[B], &faces[E], 0, 0, 0, 0
+	};
+	faces[B] = {
+			{100, 0},
+			&faces[A], &faces[B], &faces[A], &faces[B], 0, 0, 0, 0
+	};
+	faces[C] = {
+			{50, 50},
+			&faces[C], &faces[E], &faces[C], &faces[A], 0, 0, 0, 0
+	};
+	faces[D] = {
+			{0, 100},
+			&faces[E], &faces[F], &faces[E], &faces[F], 0, 0, 0, 0
+	};
+	faces[E] = {
+			{50, 100},
+			&faces[D], &faces[A], &faces[D], &faces[C], 0, 0, 0, 0
+	};
+	faces[F] = {
+			{0, 150},
+			&faces[F], &faces[D], &faces[F], &faces[D], 0, 0, 0, 0
+	};
+	return result;
+}
+
 
 using namespace std;
 
@@ -39,6 +144,7 @@ const char *facingChar = ">v<^";
 struct State {
 	Point delta;
 	Point pos;
+	const Face *face;
 	int facing = 0;
 };
 
@@ -56,33 +162,41 @@ void turn(Grid &grid, State &state, char dir) {
 	else {
 		assert(false);
 	}
-	grid[state.pos] = facingChar[state.facing];
+	Point gridPos = state.face->uv + state.pos;
+	grid[gridPos] = facingChar[state.facing];
 }
 
-void move(Grid &grid, State &state, int num) {
+void move(Grid &grid, const Map &map, State &state, int num) {
 	for (int i = 0; i < num; ++i) {
-//		grid.repr(cout, "");
+		grid[state.face->uv + state.pos] = facingChar[state.facing];
 
 		Point newPos = state.pos;
-		do {
-			newPos += state.delta;
-			newPos.mod(Point{ (int)grid.getDimMX(), (int)grid.getDimMY() });
-		} while (grid[newPos] == ' ');
 
-		if (grid[newPos] == '#') {
+		newPos += state.delta;
+
+		const Face *newFace = state.face;
+
+		if (newPos.x() >= map.tileSize || newPos.x() < 0 || newPos.y() >= map.tileSize || newPos.y() < 0) {
+			// next face...
+			newFace = state.face->neighbor[state.facing];
+			newPos.mod(Point{ map.tileSize, map.tileSize });
+		}
+
+		Point gridPos = newFace->uv + newPos;
+		assert(grid[gridPos] != ' ');
+
+		if (grid[gridPos] == '#') {
 			break; // reached wall
 		}
 
 		state.pos = newPos;
-		if (grid[state.pos] != ' ') grid[state.pos] = facingChar[state.facing];
+		state.face = newFace;
 	}
 }
 
-State walk(Grid &grid, const string &route) {
+State walk(Grid &grid, const Map &map, const string &route) {
 	string remain = route;
-	State state { {1, 0}, { 0, 0 }, 0 };
-	// move to start
-	move(grid, state, 1);
+	State state { {1, 0}, { 0, 0 }, &map.faces[0],0 };
 
 	while (remain.length() > 0) {
 		if (remain[0] == 'R' || remain[0] == 'L') {
@@ -91,7 +205,7 @@ State walk(Grid &grid, const string &route) {
 		}
 		else {
 			int num = stoi(remain);
-			move(grid, state, num);
+			move(grid, map,state, num);
 			string str = string_format("%i", num);
 			remain = remain.substr(str.length());
 		}
@@ -100,7 +214,7 @@ State walk(Grid &grid, const string &route) {
 	return state;
 }
 
-int solve1(const string &fname) {
+int solve1(const string &fname, const Map &map) {
 	ifstream infile(fname);
 	auto grid = readMap(infile);
 
@@ -108,19 +222,23 @@ int solve1(const string &fname) {
 	getline(infile, route);
 	cout << route << endl;
 
-	auto state = walk(grid, route);
+	auto state = walk(grid, map, route);
 
 	grid.repr(cout, "");
 	cout << endl;
 
 	cout << state.pos << " - " << state.facing << endl;
 
-	int result = (state.pos.y() + 1) * 1000 + (state.pos.x() + 1) * 4 + state.facing;
+	auto gridPos = state.face->uv + state.pos;
+	int result = (gridPos.y() + 1) * 1000 + (gridPos.x() + 1) * 4 + state.facing;
 	cout << result << endl;
 	return result;
 }
 
 int main() {
-//	assert(solve1("day22/test-input") == 6032);
-	cout << solve1("day22/input") << endl;
+	Map testMap = initTest();
+	assert(solve1("day22/test-input", testMap) == 6032);
+
+	Map input = initMain();
+	cout << solve1("day22/input", input) << endl; // 89224
 }
