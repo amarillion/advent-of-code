415d6f67402c27318b450339474f2afe7a3308e6
Martijn van Iersel <mvaniersel@gmail.com>
2023-12-04 18:52:45 +0100

    Clean up solution

--- 2023/day4/solution.cpp
+++ 2023/day4/solution.cpp
@@ -5,12 +5,11 @@
 #include <fstream>
 #include <vector>
 #include <set>
-#include "../common/strutil.h"
 #include <cmath>
 
 using namespace std;
 
-vector<int> processNumbers(const string &arg) {
+vector<int> parseNumbers(const string &arg) {
 	vector<int> result;
 	for (int i = 1; i < arg.length(); i += 3) {
 		auto s = arg.substr(i, 2);
@@ -25,68 +24,50 @@ vector<int> process(const string &fname) {
 	string line;
 	vector<int> result;
 
-	int i = 1;
 	while(getline(fin, line)) {
-		if (line.length() == 0) continue;
-
 		string cardContents = line.substr(line.find(':') + 1, line.length());
-		int pos = cardContents.find('|');
-		string havingStr = cardContents.substr(0, pos - 1);
-		vector<int> having = processNumbers(havingStr);
 
-		string winningStr = cardContents.substr(pos + 1, cardContents.length());
-		vector<int> winning = processNumbers(winningStr);
+		int pos = cardContents.find('|');
+		vector<int> having = parseNumbers(cardContents.substr(0, pos - 1));
+		vector<int> winning = parseNumbers(cardContents.substr(pos + 1, cardContents.length()));
 
 		vector<int> intersection;
 		set_intersection(having.begin(), having.end(), winning.begin(), winning.end(), back_inserter(intersection));
-
 		result.push_back(intersection.size());
 	}
-
 	return result;
 }
 
-int solve1(const string &fname) {
-	vector<int> data = process(fname);
+int solve1(const vector<int> &data) {
 	int result = 0;
-	int i = 1;
 	for (int d: data) {
-		int points = d == 0 ? 0 : 1 << (d-1);
-		cout << "Card: " << i++ << " intersection: " << d << " points: " << points << endl;
-		result += points;
+		result += d == 0 ? 0 : 1 << (d-1);
 	}
 	return result;
 }
 
-int solve2(const string &fname) {
+int solve2(const vector<int> &matches) {
+	int num = matches.size();
+	vector<int> counts (num, 1);
 	int result = 0;
-	vector<int> data = process(fname);
-
-	vector<int> counts;
-
-	for (int d: data) {
-		counts.push_back(1);
-	}
-
-	for (int i = 0; i < data.size(); ++i) {
-		int matches = data[i];
-		int multiplier = counts[i];
-
-//		cout << "Card " << i + 1 << " count: " << counts[i] << endl;
+	for (int i = 0; i < num; ++i) {
+		int numCopies = counts[i];
 		result += counts[i];
-		for (int j = i + 1; j < min((int)data.size(), i + matches + 1); ++j) {
-//			cout << "Adding " << multiplier  << " to " << j << endl;
-			counts[j] += (multiplier);
+		for (int j = 0; j < matches[i]; ++j) {
+			int followingCard = j + i + 1;
+			if (followingCard >= num) break;
+			counts[followingCard] += numCopies;
 		}
 	}
-
 	return result;
 }
 
 int main() {
-	assert(solve1("test-input") == 13);
-	assert(solve2("test-input") == 30);
-	cout << solve1("input") << endl; // 21158
-	cout << solve2("input") << endl; // 6050769
+	auto testData = process("test-input");
+	assert(solve1(testData) == 13);
+	assert(solve2(testData) == 30);
+	auto data = process("input");
+	assert(solve1(data) == 21158);
+	assert(solve2(data) == 6050769);
 	cout << "DONE" << endl;
 }
