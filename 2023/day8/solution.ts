2c4bbe07861a757373fb6c1fb97bdbdd2fa3f136
Martijn van Iersel <mvaniersel@gmail.com>
2023-12-08 16:48:20 +0100

    2023 Day 8, Part 2

--- 2023/day8/solution.ts
+++ 2023/day8/solution.ts
@@ -44,8 +44,60 @@ function solve1(data: Data) {
 	return steps;
 }
 
+function solve2(data: Data) {
+
+	const cycleData: Record<string, number[]> = {};
+	let pos = Object.keys(data.nodes).filter(s => s.endsWith("A"));
+	let steps = 0;
+
+	while (pos.some(s => !s.endsWith("Z"))) {
+		let instruction = data.route[steps % data.route.length];
+		const index = instruction === 'L' ? 0 : 1;
+
+		const newPos = pos.map(node => data.nodes[node][index])
+
+		let countZ = 0;
+		for (let j = 0; j < pos.length; ++j) {
+			const p = pos[j];
+			if (p.endsWith("Z")) {
+				countZ++;
+				const key = `${j}:${p}`;
+				if (key in cycleData) {
+					let prev = cycleData[key][cycleData[key].length-1];
+					cycleData[key].push(steps = prev);
+				}
+				else {
+					cycleData[key] = [ steps ];
+				}
+			}
+		}
+
+		if (steps < 10 || countZ > 0) {
+			console.log(`Step: ${steps} ${instruction} from ${pos} to ${newPos}`);
+			console.log(cycleData);
+		}
+		pos = newPos;
+		assert(pos.every(p => p in data.nodes));
+		steps++;
+		if (steps > 10_000) { break; }
+	}
+	console.log(steps);
+	return steps;
+}
+
 const testInput = parse("test-input");
 assert(solve1(testInput) === 2);
 
+const testInput2 = parse("test-input");
+assert(solve2(testInput2) === 2);
+
 const input = parse("input");
 console.log(solve1(input));
+console.log(solve2(input));
+
+// TODO: calculate GCD automatically...
+// Using online GCD tool:
+console.log(73 * 67 * 61 * 59 * 53 * 47) // Too low: 43848348119
+console.log(73 * 67 * 61 * 59 * 53 * 47 * 269) // 11795205644011
+console.log(
+	BigInt("19637") * BigInt("18023") * BigInt("16409") * BigInt("15871") * BigInt("14257") * BigInt("12643"));
