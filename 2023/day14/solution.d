853712a52a23e942cc1a434fff99533e39421d43
Martijn van Iersel <mvaniersel@gmail.com>
2024-01-07 11:40:23 +0100

    2023 Day 14 part 2

--- 2023/day14/solution.d
+++ 2023/day14/solution.d
@@ -18,10 +18,10 @@ Data parse(string fname) {
 	return readGrid(new FileReader(fname));
 }
 
-void moveUp(Data grid, Point pos) {
+void move(Data grid, Point pos, Point delta) {
 	Point current = pos;
 	while(true) {
-		Point np = current + Point(0, -1);
+		Point np = current + delta;
 		if (!grid.inRange(np)) break;
 		if (grid[np] != '.') break;
 		grid[current] = '.';
@@ -30,37 +30,119 @@ void moveUp(Data grid, Point pos) {
 	}
 }
 
-auto solve1(Data grid) {
-	// move all 'O's up
-	writeln(grid.format());
-
-	foreach(Point p; PointRange(grid.size)) {
-		if(grid[p] == 'O') {
-			moveUp(grid, p);
-		}
-	}
-
-	writeln(grid.format());
-	
-	// count positions of O's	
+long countLoad(Data grid) {
 	long result = 0;
 	foreach(Point p; PointRange(grid.size)) {
 		if(grid[p] == 'O') {
 			long val = grid.size.y - p.y;
-			writefln("Found O on %s", val);
 			result += val;
 		}
 	}
-
 	return result;
 }
 
+auto solve1(Data grid) {
+	// move all 'O's up
+	foreach(Point p; PointRange(grid.size)) {
+		if(grid[p] == 'O') {
+			move(grid, p, Point(0,-1));
+		}
+	}
+
+	// count positions of O's	
+	return countLoad(grid);
+}
+
+void spinCycle(Data grid) {
+	Point[] dirs = [Point(0, -1), Point(-1, 0), Point(0, 1), Point(1, 0)];
+	foreach(Point delta; dirs) {
+		foreach(Point p; PointRange(grid.size)) {
+			
+			Point pos = p;
+			if (delta.y > 0 || delta.x > 0) {
+				// examine in the other direction...
+				pos = grid.size - pos - 1;
+			}
+
+			if(grid[pos] == 'O') {
+				move(grid, pos, delta);
+			}
+		}
+	}
+}
+
+
+class CycleDetector {
+	long[long] lastOccurence;
+	long[long] differences;
+	void add(long pos, long val) {
+		if (val in lastOccurence) {
+			differences[val] = pos - lastOccurence[val];
+		}
+		lastOccurence[val] = pos;
+	}
+
+	long detect() {
+		writeln(differences);
+		long[long] frqMap;
+		foreach(k, v; differences) {
+			long prev = v in frqMap ? frqMap[v] : 0;
+			frqMap[v] = prev + 1;
+		}
+		long maxFrq = 0;
+		long maxPeriod = 0;
+		foreach(k, v; frqMap) {
+			if (v > maxFrq) {
+				maxFrq = v;
+				maxPeriod = k;
+			}
+		}
+		return maxPeriod;
+	}
+}
+
+long detectCycle(Data grid) {
+	auto detector = new CycleDetector();
+	for(long i = 0; i < 10_000; ++i) {
+		spinCycle(grid);
+		detector.add(i, countLoad(grid));
+	}
+	return detector.detect();
+}
+
+// we know cycle of test-input is 7...
+// cycle of input is ...
+auto solve2(Data grid, long cycleLen) {
+	long remain = 1_000_000_000;
+
+	// stabilize loop first 1000 times...
+	foreach(int i; 0..1000) {
+		spinCycle(grid);
+		remain--;
+	}
+
+	foreach(i; 0..(remain % cycleLen)) {
+		spinCycle(grid);
+	}
+
+	return countLoad(grid);
+}
+
 void main() {
-	auto testData = parse("test-input");
-	assert(solve1(testData) == 136, "Solution incorrect");
+	// auto testData = parse("test-input");
+	// assert(solve1(testData) == 136, "Solution incorrect");
+	// long testCycle = detectCycle(testData);
+	// assert(testCycle == 7); // empirical observation
+	// testData = parse("test-input");
+	// assert(solve2(testData, testCycle) == 64, "Solution incorrect");
 
 	auto data = parse("input");
-	auto result = solve1(data);
-	assert(result == 106_990);
+	// assert(solve1(data) == 106_990);
+
+	long cycle = detectCycle(data);
+	assert(cycle == 39); // empirical observation
+	data = parse("input");
+	auto result = solve2(data, cycle);
+	// assert(result == 106_990);
 	writeln(result);
 }
