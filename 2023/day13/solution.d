675a0c019c9609f51141f02096f386fe4b67b175
Martijn van Iersel <mvaniersel@gmail.com>
2024-01-07 09:52:22 +0100

    2023 Day 13 part 2

--- 2023/day13/solution.d
+++ 2023/day13/solution.d
@@ -22,47 +22,54 @@ Data parse(string fname) {
 	return result;
 }
 
-bool checkVSymmetry(Grid!(2, char) grid, int center) {
-	for (int x = 0; x < grid.width; ++x) {
+long countRangeDifferences(Range)(Range a, Range b) {
+	long result = 0;
+	while (!(a.empty || b.empty)) {
+		if (a.front != b.front) result++;
+		a.popFront();
+		b.popFront();
+	}
+	return result;
+}
+
+long countSymmetricDifferences(Grid!(2, char) grid, int center, bool isVertical) {
+	int size = isVertical ? grid.width : grid.height;
+	long result = 0;
+	for (int x = 0; x < size; ++x) {
 		int x1 = center - x;
 		int x2 = center + x + 1;
-		if (x1 < 0 || x2 >= grid.width) return true;
-		if (grid.col[x1].array != grid.col[x2].array) return false;
+		if (x1 < 0 || x2 >= size) return result;
+		if (isVertical) {
+			result += countRangeDifferences(grid.col[x1], grid.col[x2]);
+		}
+		else {
+			result += countRangeDifferences(grid.row[x1], grid.row[x2]);
+		}
 	}
-	return true;
+	return result;
 }
 
-bool checkHSymmetry(Grid!(2, char) grid, int center) {
-	for (int y = 0; y < grid.height; ++y) {
-		int y1 = center - y;
-		int y2 = center + y + 1;
-		if (y1 < 0 || y2 >= grid.height) return true;
-		if (grid.row[y1].array != grid.row[y2].array) return false;
-	}
-	return true;
-}
-
-auto solve1(Data data) {
+auto solve(Data data, long expected) {
 	long result = 0;
 	foreach(grid; data) {
 		// check for horizontal symmetry. Find consecutive matches
 		int vsymmetry = -1;
 		for (int x = 0; x < grid.size.x - 1; ++x) {
-			if (grid.col[x].array == grid.col[x+1].array) {
-				if (checkVSymmetry(grid, x)) {
-					vsymmetry = x + 1;
-					break;
-				}
+			long delta = countSymmetricDifferences(grid, x, true);
+			writefln(" Vertical: %s %s", x, delta);
+			if (delta == expected) {
+				vsymmetry = x + 1;
+				break;
 			}
 		}
 
 		int hsymmetry = -1;
 		for (int y = 0; y < grid.size.y - 1; ++y) {
-			if (grid.row[y].array == grid.row[y+1].array) {
-				if (checkHSymmetry(grid, y)) {
-					hsymmetry = y + 1;
-					break;
-				}
+			long delta = countSymmetricDifferences(grid, y, false);
+			writefln("Horizontal: %s %s", y, delta);
+			if (delta == expected) {
+				hsymmetry = y + 1;
+				break;
 			}
 		}
 
@@ -78,10 +85,13 @@ auto solve1(Data data) {
 
 void main() {
 	auto testData = parse("test-input");
-	assert(solve1(testData) == 405, "Solution incorrect");
+	assert(solve(testData, 0) == 405, "Solution incorrect");
+	assert(solve(testData, 1) == 400, "Solution incorrect");
 
 	auto data = parse("input");
-	auto result = solve1(data);
-	// assert(result == 1);
+	auto result = solve(data, 0);
+	assert(result == 31_739);
+	result = solve(data, 1);
+	// assert(result == 31_739);
 	writeln(result);
 }
