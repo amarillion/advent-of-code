52896d3a9173d1b6da7a74da34a9bf1eccaa594a
Martijn van Iersel <mvaniersel@gmail.com>
2024-01-09 20:51:05 +0100

    Day 17 WIP

--- 2023/day17/solution.d
+++ 2023/day17/solution.d
@@ -8,6 +8,7 @@ import std.array;
 import std.range;
 import std.algorithm;
 import std.typecons;
+import std.container.binaryheap;
 
 import common.io;
 import common.grid;
@@ -35,6 +36,23 @@ Tuple!(Dir,Node)[] getAdjacent(const MyGrid grid, const Node node) {
 	return result;
 }
 
+Tuple!(Dir,Node)[] getAdjacent2(const MyGrid grid, const Node node) {
+	Tuple!(Dir, Node)[] result;
+
+	foreach(dir; [Dir.E, Dir.N, Dir.W, Dir.S]) {
+		if (dir == REVERSE[node.dir]) continue; // can't reverse
+		// can't turn first four...
+		if (node.straightRemain >= 7 && dir != node.dir) continue;
+		// must turn after 10...
+		if (node.straightRemain == 0 && dir == node.dir) continue; // can't move 3 steps continuous
+		Point np = node.pos + DELTA[dir];
+		if (!grid.inRange(np)) continue;
+		result ~= tuple(dir, Node(np, dir, dir == node.dir ? node.straightRemain - 1 : 9));
+	}
+	return result;
+}
+
+
 int dijkstra(N)(N source, bool delegate(N) isDest, Tuple!(Dir,N)[] delegate(N) getAdjacent, int delegate(N) getWeight) {
 	// Mark all nodes unvisited. Create a set of all the unvisited nodes called the unvisited set.
 	// Assign to every node a tentative distance value: set it to zero for our initial node and to infinity for all other nodes. Set the initial node as current.[13]
@@ -43,30 +61,15 @@ int dijkstra(N)(N source, bool delegate(N) isDest, Tuple!(Dir,N)[] delegate(N) g
 	N[N] prev;
 	Node firstDest;
 
-	// TODO: more efficient to use a priority queue here
-	const(N)[] open = [ source ];
+	// priority queue	
+	auto open = heapify!((a, b) => dist[a] > dist[b])([ source ]);
 
 	// int maxIterations = 1000;
 	// int i = maxIterations;
 	while (open.length > 0) {
+		N current = open.front;
+		open.popFront;
 		
-		// i--; // 0 -> -1 means Infinite.
-		// if (i == 0) break;
-
-		// extract the element from Q with the lowest dist. Open is modified in-place.
-		// TODO: optionally use PriorityQueue
-		// O(N^2) like this, O(log N) with priority queue. But in my tests, priority queues only start pulling ahead in large graphs
-		N minElt;
-		bool found = false;
-		foreach (elt; open) {
-			if (!found || dist[elt] < dist[minElt]) {
-				minElt = elt;
-				found = true;
-			}
-		}
-		if (found) open = open.filter!(i => i != minElt).array;
-		
-		N current = minElt;
 		// check adjacents, calculate distance, or  - if it already had one - check if new path is shorter
 		foreach(pair; getAdjacent(current)) {
 			Node sibling = pair[1];
@@ -75,7 +78,7 @@ int dijkstra(N)(N source, bool delegate(N) isDest, Tuple!(Dir,N)[] delegate(N) g
 				int alt = dist[current] + getWeight(sibling);
 				
 				// any node that is !visited and has a distance assigned should be in open set.
-				if (!open.canFind(sibling)) open ~= sibling; // may be already in there
+				// if (!open.canFind(sibling)) open.insert(sibling); // may be already in there
 
 				int oldDist = sibling in dist ? dist[sibling] : int.max;
 
@@ -84,6 +87,8 @@ int dijkstra(N)(N source, bool delegate(N) isDest, Tuple!(Dir,N)[] delegate(N) g
 					dist[sibling] = alt;
 					// build back-tracking map
 					prev[sibling] = current;
+
+					open.insert(sibling);
 				}
 			}
 		}
@@ -121,12 +126,29 @@ auto solve1(const MyGrid grid) {
 	return result;
 }
 
+auto solve2(const MyGrid grid) {
+	Node start = Node(Point(0, 0), Dir.E, 10);
+	Point endPos = grid.size - 1;
+	auto result = dijkstra(start,
+		(Node n) => (n.pos == endPos && n.straightRemain <= 6),
+		(Node n) => getAdjacent2(grid, n),
+		(Node n) => grid[n.pos] 
+	);
+	writeln(result);
+	return result;
+}
+
 void main() {
 	auto testData = parse("test-input");
 	assert(solve1(testData) == 102, "Solution incorrect");
+	assert(solve2(testData) == 94, "Solution incorrect");
+
+	auto testData2 = parse("test-input2");
+	assert(solve2(testData2) == 71, "Solution incorrect");
 
 	auto data = parse("input");
-	auto result = solve1(data);
-	assert(result == 665);
+	assert(solve1(data) == 665);
+	auto result = solve2(data);
+	assert(result == 809);
 	writeln(result);
 }
