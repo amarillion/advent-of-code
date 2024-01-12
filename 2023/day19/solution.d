bc259f2cf29b7bbfa17f5032c0cc3c15a9a29a55
Martijn van Iersel <mvaniersel@gmail.com>
2024-01-12 11:45:20 +0100

    Day 19 cleanup: simpler binary tree representation of workflows

--- 2023/day19/solution.d
+++ 2023/day19/solution.d
@@ -12,12 +12,38 @@ import std.regex;
 
 import common.io;
 
-struct Rule {
-	bool hasCondition;
-	string left;
-	string operator;
+class Rule {
+	enum Type { CONDITION, ACCEPT, REJECT }
+	Type type;
+
+	// condition
+	char left;
+	char operator;
 	int right;
-	string dest;
+	
+	Rule ifTrue;
+	Rule ifFalse;
+
+	this(Type _type, char _left = '\0', char _operator = '\0', int _right = 0) {
+		this.type = _type; 
+		this.left = _left; 
+		this.operator = _operator; 
+		this.right = _right;
+	}
+
+	override string toString() const {
+		return toString("");
+	}
+
+	string toString(string spacer) const {
+		final switch(type) {
+			case Type.ACCEPT: return "A";
+			case Type.REJECT: return "R";
+			case Type.CONDITION: return "%s %s %s\n%s ├true: %s\n%s └false: %s".format(
+				left, operator, right, spacer, ifTrue.toString(spacer ~ " │"), spacer, ifFalse.toString(spacer ~ "  ")
+			);
+		}
+	}
 }
 
 struct Part {
@@ -28,81 +54,92 @@ struct Part {
 }
 
 struct Data {
-	Rule[][string] workflows;
+	Rule rootRule;
 	Part[] parts;
+}
+
+Rule parseRule(const string[][string] rawRules, string workflowLabel) {
+	if (workflowLabel == "A") {
+		return new Rule(Rule.Type.ACCEPT);
+	}
+	else if (workflowLabel == "R") {
+		return new Rule(Rule.Type.REJECT);
+	}
+	else {
+		Rule result = null;
+		Rule prev = null;
+		foreach(ruleString; rawRules[workflowLabel]) {
+			auto m2 = ruleString.matchFirst(regex(r"^([xmas])([><])(\d+):(\w+)$"));
+			Rule current;
+		
+			if (!m2.empty) {
+				current = new Rule(Rule.Type.CONDITION, m2[1][0], m2[2][0], to!int(m2[3]));
+				current.ifTrue = parseRule(rawRules, m2[4]);
+			}
+			else {
+				current = parseRule(rawRules, ruleString);
+			}
+
+			if (result is null) { result = current; }
+			else { prev.ifFalse = current; }
+			prev = current;
+		}
+		return result;
+	}
 }
 
 Data parse(string fname) {
 	string raw = readText(fname).stripRight;
 	string[] parts = raw.split("\n\n");
 	Data result;
+	string[][string] rawRules;
 	foreach(line; parts[0].split("\n")) {
 		auto m = line.matchFirst(regex(r"^(\w+)\{(.*)\}$"));
 		assert(!m.empty);
 		string label = m[1];
-		Rule[] rules = [];
-		
-		foreach(ruleString; m[2].split(",")) {
-			auto m2 = ruleString.matchFirst(regex(r"^([xmas])([><])(\d+):(\w+)$"));
-			if (!m2.empty) {
-				rules ~= Rule(true, m2[1], m2[2], to!int(m2[3]), m2[4]);
-			}
-			else {
-				rules ~= Rule(false, "", "", 0, ruleString);
-			}
-		}
-		result.workflows[label] = rules;
+		rawRules[label] = m[2].split(",");
 	}
+
+	result.rootRule = parseRule(rawRules, "in");
 	foreach(line; parts[1].split("\n")) {
 		auto m = line.matchFirst(regex(r"^\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}$"));
 		assert(!m.empty);
 		result.parts ~= Part(to!int(m[1]), to!int(m[2]), to!int(m[3]), to!int(m[4]));
 	}
+
+	writeln(result.rootRule.toString());
 	return result;
 }
 
-bool test(const Data data, Part part, string workflowLabel) {
-	writef(" -> %s", workflowLabel);
-	
-	if (workflowLabel == "R") {
-		return false;
-	}
-	else if (workflowLabel == "A") {
-		return true;
-	}
-
-	foreach(rule; data.workflows[workflowLabel]) {
-		if (rule.hasCondition) {
+bool test(const Rule rule, Part part) {
+	final switch(rule.type) {
+		case Rule.Type.REJECT: return false;
+		case Rule.Type.ACCEPT: return true;
+		case Rule.Type.CONDITION: { 
 			int value = 0;
 			bool result = false;
-			switch(rule.left[0]) {
+			switch(rule.left) {
 				case 'x': value = part.x; break;
 				case 'm': value = part.m; break;
 				case 'a': value = part.a; break;
 				case 's': value = part.s; break;
 				default: assert(false);
 			}
-			switch(rule.operator[0]) {
+			switch(rule.operator) {
 				case '<': result = value < rule.right; break;
 				case '>': result = value > rule.right; break;
 				default: assert(false);
 			}
-			if (result) {
-				return test(data, part, rule.dest);
-			}
-		}
-		else {
-			return test(data, part, rule.dest);
+			return result ? test(rule.ifTrue, part) : test(rule.ifFalse, part);
 		}
 	}
-	assert(false); // last rule is always unconditional... 
 }
 
 auto solve1(Data data) {
 	long result = 0;
 	foreach(part; data.parts) {
 		write(part);
-		bool testResult = test(data, part, "in");
+		bool testResult = test(data.rootRule, part);
 		writeln(" ", testResult);
 		if (testResult) {
 			result += part.x + part.m + part.a + part.s;
@@ -120,8 +157,13 @@ struct Range {
 	}
 
 	void intersect(Range other) {
-		bottomIncl = max(bottomIncl, other.bottomIncl);
-		topExcl = min(topExcl, other.topExcl);
+		if (other.bottomIncl > topExcl || other.topExcl < bottomIncl) {
+			topExcl = bottomIncl + 1;
+		}
+		else {
+			bottomIncl = max(bottomIncl, other.bottomIncl);
+			topExcl = min(topExcl, other.topExcl);
+		}
 		assert (topExcl >= bottomIncl);
 	}
 }
@@ -155,48 +197,40 @@ class PartRange {
 	}
 }
 
-long applyRange(const Data data, const PartRange partRange, string workflowLabel, int recursionLevel = 0) {
+long applyRange(const Rule rule, const PartRange partRange, int recursionLevel = 0) {
 	PartRange current = partRange.dup;
 
 	long result;
-	if (workflowLabel == "R") {
-		result = 0;
-	}
-	else if (workflowLabel == "A") {
-		result = 
+	final switch(rule.type) {
+		case Rule.Type.REJECT: return 0;
+		case Rule.Type.ACCEPT: return
 			partRange.ranges['x'].total *
 			partRange.ranges['m'].total *
 			partRange.ranges['a'].total *
 			partRange.ranges['s'].total;
-	}
-	else {
-		result = 0;
-		foreach(rule; data.workflows[workflowLabel]) {
-			if (rule.hasCondition) {
-				Range conditionFalse;
-				Range conditionTrue;
-				switch(rule.operator[0]) {
-					case '<': conditionTrue = Range(1, rule.right); conditionFalse = Range(rule.right, 4001); break;
-					case '>': conditionFalse = Range(1, rule.right + 1); conditionTrue = Range(rule.right + 1, 4001);  break;
-					default: assert(false);
-				}
-				PartRange forward = current.dup;
-				forward.ranges[rule.left[0]].intersect(conditionTrue);
-				current.ranges[rule.left[0]].intersect(conditionFalse);
-				result += applyRange(data, forward, rule.dest, recursionLevel + 1);
+		case Rule.Type.CONDITION: {
+			result = 0;
+			Range conditionFalse;
+			Range conditionTrue;
+			switch(rule.operator) {
+				case '<': conditionTrue = Range(1, rule.right); conditionFalse = Range(rule.right, 4001); break;
+				case '>': conditionFalse = Range(1, rule.right + 1); conditionTrue = Range(rule.right + 1, 4001);  break;
+				default: assert(false);
 			}
-			else {
-				result += applyRange(data, current, rule.dest, recursionLevel + 1);
-			}
+			PartRange forward = current.dup;
+			forward.ranges[rule.left].intersect(conditionFalse);
+			current.ranges[rule.left].intersect(conditionTrue);
+			result += applyRange(rule.ifTrue, current, recursionLevel + 1);
+			result += applyRange(rule.ifFalse, forward, recursionLevel + 1);
+			return result;
 		}
 	}
-	// writefln("%sWorkflow %s returning %s for part %s", ' '.repeat(recursionLevel), workflowLabel, result, partRange);
-	return result;
 }
 
 long solve2(Data data) {
 	auto partRange = new PartRange();
-	long result = applyRange(data, partRange, "in");
+	long result = applyRange(data.rootRule, partRange);
+	writeln(result);
 	return result;
 }
 
@@ -209,6 +243,6 @@ void main() {
 	auto data = parse("input");
 	assert (solve1(data) == 376_008);
 	long result = solve2(data);
-	assert(result == 124078207789312); 
+	assert(result == 124_078_207_789_312); 
 	writeln(result);
 }
