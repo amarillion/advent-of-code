4d830576a3487ee8b7cfc084ca9dbb0b47c8e4ab
Martijn van Iersel <mvaniersel@gmail.com>
2024-01-31 21:36:54 +0100

    Improve run-all

--- 2023/run-all.d
+++ 2023/run-all.d
@@ -5,6 +5,7 @@ import std.process;
 import std.stdio;
 import std.format;
 import std.string;
+import core.time;
 
 struct Test {
 	int year;
@@ -23,6 +24,8 @@ Test[] tests = [
 	Test(2023,  3,  "day3/", "./solution.cpp", "test-input", "4361\n467835"),
 	Test(2023,  4,  "day4/", "./solution.cpp", "test-input", "13\n30"),
 	Test(2023,  5,  "day5/", "./solution.cpp", "test-input", "35\n46"),
+	Test(2023,  6,  "day6/", "./Solution",     "test-input", "288\n71503"),
+	Test(2023,  7,  "day7/", "./Solution",     "test-input", "6440\n5905"),
 	
 	Test(2023,  9,  "day9/", "./Solution", "test-input", "114\n2"),
 	
@@ -34,13 +37,43 @@ Test[] tests = [
 	Test(2023,  3,  "day3/", "./solution.cpp", "input", "521515\n69527306"),
 	Test(2023,  4,  "day4/", "./solution.cpp", "input", "21158\n6050769"),
 	Test(2023,  5,  "day5/", "./solution.cpp", "input", "324724204\n104070862"),
-	
+	Test(2023,  6,  "day6/", "./Solution",     "input", "220320\n34454850"),
+	Test(2023,  7,  "day7/", "./Solution",     "input", "248453531\n248781813"),
+	/*
+
+
+	public static void main(String[] args) throws IOException {
+		var testData1 = parse1(Path.of("day6/test-input"));
+		assertEqual(solve1(testData1), 288);
+
+		var testData2 = parse2(Path.of("day6/test-input"));
+		assertEqual(solve1(testData2), 71503);
+
+		var data1 = parse1(Path.of("day6/input"));
+		System.out.println(solve1(data1));
+
+		var data2 = parse2(Path.of("day6/input"));
+		System.out.println(solve1(data2));
+
+
+*/
+
+/*
+		var testData = parse(Path.of("day7/test-input"));
+		Util.assertEqual(solve1(testData), 6440);
+		Util.assertEqual(solve2(testData), 5905);
+		var data = parse(Path.of("day7/input"));
+		System.out.println(solve1(data)); // 248453531
+		System.out.println(solve2(data)); // 248781813
+*/
 	Test(2023,  9,  "day9/", "./Solution", "input", "2043677056\n1062"),
 	
 	Test(2023, 18, "day18/", "./solution.d", "input", "[31171, 131431655002266]"),
 ];
 
 void runTest(Test t) {
+
+	MonoTime before = MonoTime.currTime;
 	writefln("Executing script %s %s", t.year, t.day);
 	auto execResult = executeShell(format("%s %s", t.script, t.param), null, Config.none, size_t.max, t.workDir);
 	assert(execResult.status == 0, format("Script failed with status code %s", execResult.status));
@@ -48,6 +81,10 @@ void runTest(Test t) {
 	assert(execResult.output.strip == t.expected, 
 		format("Did not receive expected results")
 	);
+	MonoTime after = MonoTime.currTime;
+	Duration timeElapsed = after - before;
+	auto elapsedFormat = timeElapsed.split!("minutes", "seconds", "msecs");
+	writefln("Elapsed: %02d:%02d.%03d\n\n", elapsedFormat.minutes, elapsedFormat.seconds, elapsedFormat.msecs);
 }
 
 void main(string[] args) {
