488eed79537f474e7cb1ca8807d8d324e1485ada
Martijn van Iersel <mvaniersel@gmail.com>
2024-02-17 20:38:04 +0100

    2021 day 23: Bring it all back in one file

--- 2021/day23/solution.d
+++ 2021/day23/solution.d
@@ -4,8 +4,290 @@ module day23.solution;
 import std.stdio;
 import common.io;
 
-import part1 = day23.part1;
-import part2 = day23.part2;
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.concurrency;
+import std.format;
+import std.math;
+import std.range;
+import std.stdio;
+import std.typecons;
+
+import common.io;
+import common.vec;
+import common.dijkstra;
+import common.astar;
+import common.util;
+import common.coordrange;
+import common.grid;
+
+enum int[char] podCosts = [
+	'A': 1, 'B': 10, 'C': 100, 'D': 1000
+];
+
+enum int[char]roomx = [
+	'A': 3, 'B': 5, 'C': 7, 'D': 9
+];
+
+struct Pod {
+	char type;
+	int pos;
+
+	this(char type, int pos) {
+		assert(['A', 'B', 'C', 'D'].canFind(type), "Wrong type " ~ type);
+		this.type = type;
+		this.pos = pos;
+	}
+}
+
+struct Move {
+	int cost;
+	Pod from;
+	int to;
+}
+
+struct Location {
+	int id;
+	Point pos;
+	bool isHallway;
+	bool isForbidden;
+	char type = '.';
+	int[] adjacent;
+	int[char] distanceHome;
+	int nextInRoom = -1;
+}
+
+alias Map = Location[];
+
+Map readMap(Grid!char grid) {
+	
+	Location[Point] locationByPos;
+	int roomHeight = to!int(grid.size.y) - 3;
+	int hallwayWidth = to!int(grid.size.x) - 2;
+
+	// writeln(grid.format(""));
+	foreach(pos; PointRange(grid.size)) {
+		char c = grid.get(pos);
+		if (".ABCD".canFind(c)) {
+			Location loc;
+			loc.pos = pos;
+
+			// start with distance home through the hallway
+			foreach(char type, int xx; roomx) {
+				loc.distanceHome[type] = abs(pos.x - xx) + 1;
+			}
+			if (pos.y == 1) {
+				loc.isHallway = true;
+				loc.id = pos.x - 1;
+			}
+			else if (pos.y > 1) {
+				int roomIndex = (pos.x - 3) / 2; // where 0 is A, 1 is B, etc.
+				int posInRoom = pos.y - 2;
+				loc.type = "ABCD"[(pos.x - 3) / 2];
+				
+				// backward-compatible way to assign ids to locations... can be simplified after transition done.
+				loc.id = hallwayWidth + posInRoom + roomIndex * roomHeight;
+
+				if (posInRoom + 1 < roomHeight) {
+					loc.nextInRoom = loc.id + 1;
+				}
+
+				foreach(char type, int xx; roomx) {
+					// if we're already home, then distance home is zero.
+					if (type == loc.type) { loc.distanceHome[type] = 0; }
+					// else add the vertical distance through the room we're in.
+					else { loc.distanceHome[type] += posInRoom + 1; }
+				}
+			}
+			locationByPos[pos] = loc;
+		}
+	}
+
+	// find adjacents, and convert to map by id.
+	Map result;
+	result.length = locationByPos.length;
+	foreach(pos, loc; locationByPos) {
+		foreach(delta; [Point(1, 0), Point(0, 1), Point(-1, 0), Point(0, -1)]) {
+			Point np = pos + delta;
+			if (np in locationByPos) {
+				loc.adjacent ~= locationByPos[np].id;
+			}
+		}
+		sort(loc.adjacent);
+		if (loc.adjacent.length >= 3) {
+			loc.isForbidden = true;
+		}
+		result[loc.id] = loc;
+	}
+
+	// foreach(loc; result) {
+	// 	writefln("%s: %s %s %s", loc.id, loc.pos, loc.nextInRoom, loc.adjacent);
+	// }
+
+	return result;
+}
+
+struct Data {
+	Map map;
+	Pod[] initialPods;
+	Pod[] goalPods;
+}
+
+// calculate cost of putting everyting in its right state...
+int heuristic(State)(State state, ref Map map) {
+	int result = 0;
+	foreach(pod; state) {
+		result += map[pod.pos].distanceHome[pod.type] * podCosts[pod.type];
+	}
+	return result;
+}
+
+Data parse(string[] lines) {
+	Point size = Point(to!int(lines[0].length), to!int(lines.length));
+	
+	Grid!char grid = new Grid!char(size.x, size.y);
+	foreach(pos; PointRange(grid.size)) {
+		string line = lines[pos.y];
+		char ch = pos.x < line.length ? line[pos.x] : ' ';
+		grid.set(pos, ch);
+	}
+
+	Map map = readMap(grid);
+
+	Pod[] pods;
+	Pod[] goalPods;
+	foreach(id, loc; map) {
+		if (loc.isHallway) continue;
+
+		// read original pod from map
+		pods ~= Pod(grid.get(loc.pos), to!int(id));
+		// also track target pod for this point
+		goalPods ~= Pod(loc.type, to!int(id));
+	}
+	sort!"a.pos < b.pos"(goalPods);
+	sort!"a.pos < b.pos"(pods);
+
+	return Data(map, pods, goalPods);
+}
+
+
+void sortPods(State)(ref State state) {
+	sort!((a, b) => a.pos < b.pos)(state[]);
+}
+
+bool isEndCondition(State)(State state, ref Map map) {
+	foreach(Pod p; state) {
+		if (map[p.pos].isHallway) return false;
+		if (map[p.pos].type != p.type) return false;
+	}
+	return true;
+}
+
+bool targetRoomMismatch(State)(State state, int type, ref Map map) {
+	foreach(p; state) {
+		// ignore pods that are in the hallway
+		if (map[p.pos].isHallway) continue;
+		// for all the pods that are in the room of the target type
+		if (map[p.pos].type != type) continue;
+
+		// is that pod in the right room?
+		if (p.type != type) return false;
+	}
+	return true;
+}
+
+Tuple!(Move, State)[] validMoves(State)(State state, ref Map map) {
+	Tuple!(Move, State)[] result;
+	// create a position map
+	char[int] occupancy;
+	foreach (Pod p; state) {
+		occupancy[p.pos] = p.type;
+	}
+
+	foreach (int ii, Pod p; state) {
+
+		int[] adjFunc(int i) {
+			return map[i].adjacent.filter!(j => j !in occupancy).array;
+		}
+
+		// calculate cost for all Edges where this can go...
+		auto dijk = dijkstra!(int)(p.pos, (int) => false, &adjFunc, (int) => podCosts[p.type]);
+
+		foreach(dest; dijk.prev.keys) {
+			State newState = state;
+			newState[ii] = Pod(p.type, dest);
+			sortPods(newState);
+
+			bool podInHallway = map[p.pos].isHallway;
+			bool destInHallway = map[dest].isHallway;
+			// never stop on t-section
+			if (map[dest].isForbidden) continue;
+			// don't move within hallway
+			if (podInHallway && destInHallway) continue;
+			// don't move to room unless it's the destination
+			if (!destInHallway && map[dest].type != p.type) continue;
+			// EXTRA CONDITION: destination must not contain mismatches
+			if (!destInHallway && !targetRoomMismatch(newState, p.type, map)) continue;
+			// EXTRA CONDITION to reduce search space: don't move within a room
+			if (!podInHallway && !destInHallway && map[p.pos].type == map[dest].type) continue;
+			// EXTRA CONDITION to reduce search space: if we're in a room, check that the next spot isn't empty
+			if (map[dest].nextInRoom > 0 && map[dest].nextInRoom !in occupancy) continue;
+
+			int cost = dijk.dist[dest];
+			result ~= tuple(Move(cost, p, dest), newState);
+		}
+	}
+	return result;
+}
+
+alias State8 = Pod[8];
+alias Edge8 = Tuple!(Move, State8);
+
+auto solve1(string[] lines) {
+	auto data = parse(lines);
+
+	State8 state = to!(Pod[8])(data.initialPods);
+	sortPods(state);
+
+	State8 goal = to!(Pod[8])(data.goalPods);
+	assert(goal.isEndCondition(data.map));
+
+	auto astarResult = astar!(State8, Move)(
+		state, 
+		s => s == goal, 
+		s => s.validMoves!State8(data.map), 
+		(Edge8 m) => m[0].cost,
+		s => s.heuristic(data.map)
+	);
+	auto current = goal;
+	assert(current in astarResult.prev);
+	return astarResult.prev[goal].cost;
+}
+
+alias State16 = Pod[16];
+alias Edge16 = Tuple!(Move, State16);
+
+auto solve2(string[] lines) {
+	auto data = parse(lines);
+
+	State16 state = to!(Pod[16])(data.initialPods);
+	sortPods(state);
+
+	State16 goal = to!(Pod[16])(data.goalPods);
+	assert(goal.isEndCondition(data.map));
+
+	auto astarResult = astar!(State16, Move)(
+		state, 
+		s => s == goal, 
+		s => s.validMoves!State16(data.map), 
+		(Edge16 m) => m[0].cost,
+		s => s.heuristic(data.map)
+	);
+	auto current = goal;
+	assert(current in astarResult.prev);
+	return astarResult.prev[goal].cost;
+}
 
 void main(string[] args) {
 	assert(args.length == 2, "Argument expected: input file");
@@ -14,7 +296,7 @@ void main(string[] args) {
 	string[] lines2 = lines[0..3] ~ ["  #D#C#B#A#", "  #D#B#A#C#" ] ~ lines[3..$];
 
 	writeln ([
-		part1.solve(lines),
-		part2.solve(lines2)
+		solve1(lines),
+		solve2(lines2)
 	]);
 }
