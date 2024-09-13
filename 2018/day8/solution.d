6ee37ab246f0fc82186d0d1240ab0071e18a9afa
Martijn van Iersel <mvaniersel@gmail.com>
2024-09-13 08:43:51 +0200

    2018, day 8, clean up

--- 2018/day8/solution.d
+++ 2018/day8/solution.d
@@ -11,76 +11,50 @@ import std.algorithm;
 import common.io;
 import core.stdcpp.array;
 
-alias Data = int[];
-Data parse(string fname) {
+Node parse(string fname) {
 	string[] lines = readLines(fname);
-	return lines[0].split(" ").map!(to!int).array;
+	int[] data = lines[0].split(" ").map!(to!int).array;
+	return processNode(data);
 }
-
 
 class Node {
 	Node[] children;
 	int[] metaData;
-
-	string repr(string indent = "") {
-		return indent 
-			~ to!string(metaData) ~ " " ~ to!string(value()) ~ "\n" 
-			~ children.map!(c => c.repr(indent ~ " ")).join("");
-	}
-
-	int sum() {
-		return metaData.sum + children.map!(c => c.sum).sum;
-	}
-
-	int value() {
-		int result;
-		if (children.empty) { 
-			result = metaData.sum;
-		}
-		else {
-			result = metaData.map!(i => (i <= children.length ? children[i - 1].value : 0)).sum;
-		}
-		writeln(result);
-		return result;
-	}
-
 }
 
-class Parser {
-	this(Data data) {
-		remain = data.dup;
-	}
-	
-	Data remain;
+int recursiveSum(Node n) {
+	return n.metaData.sum + n.children.map!(c => recursiveSum(c)).sum;
+}
 
-	// recursive descent parser...
-	Node parseNode() {
-		Node result = new Node();
-		int numChildren = remain[0];
-		int numMetaData = remain[1];
-		remain = remain[2..$];
-		foreach(i; 0..numChildren) {
-			result.children ~= parseNode();
-		}
-		result.metaData = remain[0..numMetaData];
-		remain = remain[numMetaData..$];
-		return result;
+int recursiveValue(Node n) {
+	if (n.children.empty) { 
+		return n.metaData.sum;
+	}
+	else {
+		return n.metaData.map!(i => (i <= n.children.length ? n.children[i - 1].recursiveValue : 0)).sum;
 	}
 }
 
-auto recursiveDescent(Data data) {
-	writeln(data);
-	auto parser = new Parser(data);
-	Node node = parser.parseNode();
-	writeln(node.repr(""));
-	return node;
+/*
+ * Consume a chunk of `remain` and return it as a Node.
+ * Recursive.
+ */
+Node processNode(ref int[] remain) {
+	Node result = new Node();
+	int numChildren = remain[0];
+	int numMetaData = remain[1];
+	remain = remain[2..$];
+	foreach(i; 0..numChildren) {
+		result.children ~= processNode(remain);
+	}
+	result.metaData = remain[0..numMetaData];
+	remain = remain[numMetaData..$];
+	return result;
 }
-
 
 void main(string[] args) {
 	assert(args.length == 2, "Expecting 1 argument: input file");
-	auto data = parse(args[1]);
-	auto node = recursiveDescent(data);
-	writeln(node.sum);
-	writeln(node.value);
+	auto node = parse(args[1]);
+	writeln(recursiveSum(node));
+	writeln(recursiveValue(node));
 }
