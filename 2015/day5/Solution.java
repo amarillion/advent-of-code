043a2cdf290062261ce6220407a4468cc38a00e4
Martijn van Iersel <mvaniersel@gmail.com>
2024-09-15 11:15:39 +0200

    2015, Day 5, part 2

--- 2015/day5/Solution.java
+++ 2015/day5/Solution.java
@@ -43,10 +43,42 @@ public class Solution {
 		return result;
 	}
 
+	private static long solve2(List<String> data) {
+		long result = 0;
+		for(String line: data) {
+			int vowels = 0;
+			boolean hasRepeatingLetter = false;
+			boolean hasRepeatingPair = false;
+			for(int i = 2; i < line.length(); ++i) {
+				if (line.charAt(i-2) == line.charAt((i))) {
+					hasRepeatingLetter = true;
+					break;
+				}
+			}
+
+			for(int i = 1; i < line.length() - 1; ++i) {
+				String pair = line.substring(i-1, i+1);
+				if (line.substring(i + 1).contains(pair)) {
+					hasRepeatingPair = true;
+					break;
+				}
+			}
+
+//			System.out.println(line + " " + vowels + " " + hasDoubleLetters + " " + noForbiddenPairs);
+			if (hasRepeatingPair && hasRepeatingLetter) {
+				result++;
+			}
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) throws IOException {
 		var testData = parse(Path.of("day5/test-input"));
 		Util.assertEqual(solve1(testData), 2);
 		var data = parse(Path.of("day5/input"));
 		System.out.println(solve1(data));
+
+		System.out.println(solve2(data));
 	}
 }
