2513eaef27909510e28665f895f85bd9542098b4
Martijn van Iersel <mvaniersel@gmail.com>
2024-12-04 08:37:29 +0100

    2024 day 4 part 2

--- 2024/day4/solution.ts
+++ 2024/day4/solution.ts
@@ -10,18 +10,17 @@ function parse(fname: string) {
 	return data;
 }
 
-function search(data: string[], x: number, y: number, dx: number, dy: number) {
-	
-	function inRange(x: number, y: number) {
-		return x >= 0 && y >= 0 && x < data[0].length && y < data.length; 
-	}
+function inRange(data, x: number, y: number) {
+	return x >= 0 && y >= 0 && x < data[0].length && y < data.length; 
+}
 
+function search(data: string[], x: number, y: number, dx: number, dy: number) {
 	let NEEDLE = 'XMAS';
 	let xx = x;
 	let yy = y;
 
 	for (let pos = 0; pos < 4; ++pos) {
-		if (!inRange(xx, yy)) return false;
+		if (!inRange(data, xx, yy)) return false;
 		if(data[yy][xx] !== NEEDLE[pos]) return false;
 		xx += dx;
 		yy += dy;
@@ -31,7 +30,6 @@ function search(data: string[], x: number, y: number, dx: number, dy: number) {
 
 function solve1(data: Data) {
 	let result = 0;
-
 
 	for (let y = 0; y < data.length; ++y) {
 		for (let x = 0; x < data[0].length; ++x) {
@@ -53,12 +51,30 @@ function solve1(data: Data) {
 	return result;
 }
 
-// function solve2(data: Data) {
-// 	let result = 0;
-// 	return result;
-// }
+function searchCross(data: string[], x: number, y: number, dx: number, dy: number) {
+	let pair = [ data[y + dy][x + dx], data[y][x], data [y - dy][x - dx] ].join('');
+	if (!(pair === 'MAS' || pair === 'SAM')) return false;
+	[dx, dy] = [-dy, dx];
+	pair = [ data[y + dy][x + dx], data[y][x], data [y - dy][x - dx] ].join('');
+	return (pair === 'MAS' || pair === 'SAM');
+}
+
+function solve2(data: Data) {
+	let result = 0;
+	for (let y = 1; y < data.length-1; ++y) {
+		for (let x = 1; x < data[0].length-1; ++x) {
+			if (data[y][x] === 'A') {
+				if (searchCross(data, x, y, 1, 1)) {
+					console.log(x, y, 1, 1);
+					result++;
+				}
+			}
+		}
+	}
+	return result;
+}
 
 assert(process.argv.length === 3, 'Expected argument: input filename');
 const data = parse(process.argv[2]);
 console.log(solve1(data));
-// console.log(solve2(data));
+console.log(solve2(data));
