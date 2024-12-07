bb6622ab6baf6cf4b863d7511c2bb16f5b957f43
Martijn van Iersel <mvaniersel@gmail.com>
2024-12-07 21:40:14 +0100

    2024, Day 7 part 2

--- 2024/day7/solution.ts
+++ 2024/day7/solution.ts
@@ -38,12 +38,42 @@ function solve1(data: Data) {
 	return result;
 }
 
-// function solve2(data: Data) {
-// 	let result = 0;
-// 	return result;
-// }
+function hasPossibleResultsExt(expected: number, inputs: number[]) {
+	// console.log("Checking", { expected, inputs });
+	assert(inputs.length > 0);
+	if (inputs.length === 1) {
+		return expected === inputs[0];
+	}
+	const remain = inputs.slice();
+	const first = remain.pop();
+	assert(first);
+	const expectedStr = `${expected}`;
+	const firstStr = `${first}`;
+	if (`${expected}`.endsWith(`${first}`)) {
+		if (hasPossibleResultsExt(Number(expectedStr.substring(0, expectedStr.length - firstStr.length)), remain)) {
+			return true;
+		}
+	}
+	return hasPossibleResultsExt(expected - first, remain) ||
+		hasPossibleResultsExt(expected / first, remain);
+}
+
+// 7290
+// 7290 / 15 = 486
+// endsWith first
+
+function solve2(data: Data) {
+	let result = 0;
+	for (const row of data) {
+		if (hasPossibleResultsExt(row.answer, row.inputs)) {
+			// console.log("Match", row);
+			result += row.answer;
+		}
+	}
+	return result;
+}
 
 assert(process.argv.length === 3, 'Expected argument: input filename');
 const data = parse(process.argv[2]);
 console.log(solve1(data));
-// console.log(solve2(data));
+console.log(solve2(data));
