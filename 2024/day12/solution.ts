2cd8842c66b3a5768c3f93e8980d026826e7c411
Martijn van Iersel <mvaniersel@gmail.com>
2024-12-13 19:04:24 +0100

    2024 day 12 part 2

--- 2024/day12/solution.ts
+++ 2024/day12/solution.ts
@@ -2,15 +2,18 @@
 
 import { readFileSync } from 'fs';
 import { assert } from '../common/assert.js';
-import { eachRange, Grid, readGridFromFileEx } from '../common/grid.js';
+import { eachRange, Grid, inRange, readGridFromFileEx } from '../common/grid.js';
 import { TemplateGrid } from '@amarillion/helixgraph/lib/BaseGrid.js';
 import { bfsGenerator } from '@amarillion/helixgraph/lib/pathFinding.js';
+import { Point } from '../common/point.js';
 
 type RegionType = {
 	id: number;
 	area: number;
 	char: string;
 	permiter: number;
+	corners: number;
+	start: Point;
 }
 
 type Data = TemplateGrid<Cell>;
@@ -29,8 +32,26 @@ function parse(fname: string) {
 	return new TemplateGrid<Cell>(data[0].length, data.length, (x, y) => new Cell(x, y, data[y][x]));
 }
 
+
+const NORTH = 1;
+const EAST = 2;
+const SOUTH = 4;
+const WEST = 8;
+
+const DELTAS = {
+	[NORTH]: new Point(0, -1),
+	[EAST]: new Point(1, 0),
+	[SOUTH]: new Point(0, 1),
+	[WEST]: new Point(-1, 0)
+};
+const TURN = {
+	[NORTH]: EAST,
+	[EAST]: SOUTH,
+	[SOUTH]: WEST,
+	[WEST]: NORTH
+};
+
 function solve1(grid: Data) {
-	let result = 0;
 	let nextRegion = 1;
 	const regions: RegionType[] = [];
 
@@ -41,7 +62,9 @@ function solve1(grid: Data) {
 				id: nextRegion++,
 				permiter: 0,
 				area: 0,
-				char: cell.char
+				char: cell.char,
+				corners: 0,
+				start: new Point(cell.x, cell.y)
 			}
 			regions.push(region);
 			nextRegion++;
@@ -61,17 +84,109 @@ function solve1(grid: Data) {
 				regionCell.region = region;
 				region.area++;
 				region.permiter += localPerimiter;
-				// TODO calculate perimiter
 			}
 		}
 	});
 
+	eachRange(grid.width, grid.height, (x, y) => {
+		const data: boolean[] = [];	
+		const region = grid.get(x, y).region!;
+		for (const [dx, dy] of [
+			[0, -1],
+			[1, -1],
+			[1, 0],
+			[1, 1],
+			[0, 1],
+			[-1, 1],
+			[-1, 0],
+			[-1, -1]
+		]) {
+			const newPos = new Point(x + dx, y + dy);
+			data.push(grid.inRange(newPos.x, newPos.y) && grid.get(newPos.x, newPos.y).region === region);
+		}
+		const prev = region.corners;
+		if (!data[0] && /* !data[1] && */ !data[2]) {
+			region.corners++;
+		}
+		if (data[0] && !data[1] && data[2]) {
+			region.corners++;
+		}
+		if (!data[2] && /* !data[3] && */ !data[4]) {
+			region.corners++;
+		}
+		if (data[2] && !data[3] && data[4]) {
+			region.corners++;
+		}
+		if (!data[4] && /* !data[5] && */ !data[6]) {
+			region.corners++;
+		}
+		if (data[4] && !data[5] && data[6]) {
+			region.corners++;
+		}
+		if (!data[6] && /* !data[7] && */ !data[0]) {
+			region.corners++;
+		}
+		if (data[6] && !data[7] && data[0]) {
+			region.corners++;
+		}
+		if (region.corners !== prev) {
+			console.log(`Corners: ${region.corners-prev} ${region.char}: ${x},${y} ${data.map(i => i ? 1 : 0).join('')}`);
+		}
+	});
+	function wallhugger(region: RegionType) {
+		let pos = region.start;
+		const corners = new Set<string>();
+		const states = new Set<string>();
+		let dir = SOUTH;
+		while(true) {
+			const forwardPos = Point.plus(pos, DELTAS[dir]);
+			const rightPos = Point.plus(pos, DELTAS[TURN[dir]]);
+			const leftPos = Point.plus(pos, DELTAS[TURN[TURN[TURN[dir]]]]);
+
+			const forwardCell = (grid.inRange(forwardPos.x, forwardPos.y) && grid.get(forwardPos.x, forwardPos.y).region === region);
+			const rightCell = (grid.inRange(rightPos.x, rightPos.y) && grid.get(rightPos.x, rightPos.y).region === region);
+
+			if (rightCell) {
+				// turn right
+				corners.add(`${pos.x},${pos.y};${dir}`);
+				pos = rightPos;
+				dir = TURN[dir];
+			}
+			else if (forwardCell) {
+				// move forward
+				pos = forwardPos;
+			}
+			else {
+				// turn left
+				corners.add(`${pos.x},${pos.y};${dir}`);
+				dir = TURN[TURN[TURN[dir]]];
+			}
+
+			const state = `${pos.x},${pos.y}-${dir}`;
+			if (states.has(state)) {
+				break;
+			}
+			else {
+				states.add(state);
+			}
+		}
+		console.log(corners);
+		return corners.size;
+	}
+
+	// for (const region of regions) {
+	// 	region.corners = wallhugger(region);
+	// }
+
 	// for each region
+	let result = 0;
+	let result2 = 0;
 	for (const region of regions) {
 		console.log(region);
 		result += (region.area * region.permiter);
+		result2 += (region.area * region.corners);
 	}
-	return result;
+	return [result, result2];
 }
 
 // function solve2(data: Data) {
@@ -83,3 +198,6 @@ assert(process.argv.length === 3, 'Expected argument: input filename');
 const data = parse(process.argv[2]);
 console.log(solve1(data));
 // console.log(solve2(data));
+
+// 886164 too low
+// test-input4 wrong...
