415afa0cc02afc2b3cff6d047d1b60ecbd5069d4
Martijn van Iersel <mvaniersel@gmail.com>
2025-01-09 23:27:26 +0100

    Day 17 part 2

--- 2024/day17/solution.ts
+++ 2024/day17/solution.ts
@@ -2,6 +2,7 @@
 
 import { readFileSync } from 'fs';
 import { assert } from '../common/assert.js';
+import { astar } from '@amarillion/helixgraph';
 
 type Data = ReturnType<typeof parse>;
 
@@ -16,9 +17,7 @@ function parse(fname: string) {
 }
 
 function solve1({ program, registers }: Data) {
-	let result = 0;
 	let ip = 0;
-
 	function asCombo(operand: number) {
 		if (operand < 4) return operand;
 		switch(operand) {
@@ -75,17 +74,55 @@ function solve1({ program, registers }: Data) {
 				break;
 			default: assert(false);
 		}
+		// console.log(`A=${registers.A.toString(2)}, B=${registers.B.toString(2)}, C=${registers.C.toString(2)} ${out}`)
 	}
 	return out.join(',');
 }
 
-// function solve2(data: Data) {
-// 	let result = 0;
-// 	return result;
-// }
+// hard-coded version of my program input.
+function solve1b(initial: bigint) {
+	let a = initial, b = 0n, c = 0n;
+	let out: bigint[] = [];
+	while (a !== 0n) {
+		b = a % 8n;
+		b = b ^ 2n;
+		c = a >> b;
+		b = b ^ c;
+		a = a >> 3n;
+		b = b ^ 7n;
+		out.push(b % 8n);
+	}
+	return out;
+}
+
+function solve2(data: Data) {
+	let a = (1n << 47n);
+	astar(a, 0n, function *(a: bigint) {
+			for (let i = 0n; i < 48n; ++i) {
+				let y = a ^ (1n << i);
+				yield [i, y];
+			}
+		}, { getWeight: () => 0, getHeuristic: (a: bigint) => {			
+			let matches = countResult(data, a);
+			assert (matches < 16, `${a}`);
+			return 16 - matches;
+		}}
+	);
+}
+
+function countResult(data: Data, a: bigint) {
+	let matches = 0;
+	let result = solve1b(a);
+	for(let i = data.program.length - 1; i >= 0; i--) {
+		if (result[i] === BigInt(data.program[i])) { matches++; }
+		// else { break; }
+	}
+	// console.log(`${a.toString(2)} ${a} ${result.join(',')} ${matches}`);
+	return matches;
+}
 
 assert(process.argv.length === 3, 'Expected argument: input filename');
 const data = parse(process.argv[2]);
-console.log(data);
-console.log(solve1(data));
-// console.log(solve2(data));
+
+console.log(solve1(structuredClone(data)));
+console.log(solve2(data));
