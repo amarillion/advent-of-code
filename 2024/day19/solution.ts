e90cc11982f2c580a3b5dcacc463931742477474
Martijn van Iersel <mvaniersel@gmail.com>
2025-01-16 16:41:52 +0100

    2024 day 19 part 2

--- 2024/day19/solution.ts
+++ 2024/day19/solution.ts
@@ -68,6 +68,41 @@ function isPossibleDyn(startPattern: string, available: Set<string>) {
 	return helper(startPattern);
 }
 
+// TODO: extract memoization...
+function countRecursive(startPattern: string, available: Set<string>) {
+	
+	const cache = new Map<string, number>();
+	const minLength = [...data.available].map(str => str.length).reduce((cur, acc) => Math.min(cur, acc), Infinity);
+
+	function helper(pattern: string, indent = '') {
+		if (cache.has(pattern)) {
+			return cache.get(pattern)!;
+		}
+
+		let result = 0;
+		if (available.has(pattern)) {
+			cache.set(pattern, 1);
+			result += 1;
+		}
+
+		if (pattern.length > minLength) {
+			for (let i = minLength; i <= pattern.length - minLength; ++i) {
+				const pat1 = pattern.substring(0, i);
+				const pat2 = pattern.substring(i);
+				if (available.has(pat1)) {
+					const count = helper(pat2, indent + ' ');
+					result += count;
+				}
+			}
+		}
+		cache.set(pattern, result);
+		return result;
+	
+	}
+
+	return helper(startPattern);
+}
+
 function solve1(data: Data) {
 	let result = 0;
 	for (const pattern of data.desired) {
@@ -78,12 +113,17 @@ function solve1(data: Data) {
 	return result;
 }
 
-// function solve2(data: Data) {
-// 	let result = 0;
-// 	return result;
-// }
+function solve2(data: Data) {
+	let result = 0;
+	for (const pattern of data.desired) {
+		const count = countRecursive(pattern, data.available);
+		console.log(pattern, count);
+		result += count;
+	}
+	return result;
+}
 
 assert(process.argv.length === 3, 'Expected argument: input filename');
 const data = parse(process.argv[2]);
 console.log(solve1(data));
-// console.log(solve2(data));
+console.log(solve2(data));
