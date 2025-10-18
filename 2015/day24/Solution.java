18e640bef8a2558e4ec05e19f8eb36487644d420
Martijn van Iersel <mvaniersel@gmail.com>
2025-10-18 11:29:58 +0200

    2015 day 24 part 2

--- 2015/day24/Solution.java
+++ 2015/day24/Solution.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
@@ -85,11 +86,44 @@ public class Solution {
 		return 0;
 	}
 
+	private static List<Integer> splitEqually(Set<Integer> validSubsets, int remainder, int n) {
+		if (n == 1) {
+			if (validSubsets.contains(remainder)) {
+				return new ArrayList<>(remainder);
+			}
+			else {
+				return null;
+			}
+		}
+
+		// search for another subset that fully overlaps with remainder
+		for (int s2 : validSubsets) {
+			if ((remainder & s2) == s2) {
+				// found second subset
+				List<Integer> result = splitEqually(validSubsets, remainder - s2, n - 1);
+				if (result != null) {
+					result.add(s2);
+					return result;
+				}
+			}
+		}
+
+		return null;
+	}
+
 	private static long solve1(List<Integer> data) {
+		return equalDivisionSmallestQe(data, 3);
+	}
+
+	private static long solve2(List<Integer> data) {
+		return equalDivisionSmallestQe(data, 4);
+	}
+
+	private static long equalDivisionSmallestQe(List<Integer> data, int divisor) {
 		int totalSize = data.size();
 		int totalWeight = data.stream().mapToInt(Integer::intValue).sum();
-		assert (totalWeight % 3 == 0);
-		int packageWeight = totalWeight / 3;
+		assert (totalWeight % divisor == 0);
+		int packageWeight = totalWeight / divisor;
 
 		int numPossibleSubsets = (1 << totalSize);
 		int fullSet = numPossibleSubsets - 1;
@@ -109,7 +143,7 @@ public class Solution {
 			subsetSize.put(subset, size);
 		}
 
-		System.out.println("Total number of possible subsets: " + subsetWeight.size());
+//		System.out.println("Total number of possible subsets: " + subsetWeight.size());
 
 		// sort by size, ascending
 		List<Integer> sortedSubsets = new ArrayList<>(subsetSize.keySet());
@@ -127,14 +161,14 @@ public class Solution {
 
 			int remainder = fullSet - subset;
 
-			int s2 = divideInTwo(subsetSize.keySet(), remainder);
-			if (s2 != 0) {
-				int s3 = remainder - s2;
+			List<Integer> remainingSubsets = splitEqually(subsetSize.keySet(), remainder, (divisor - 1));
+			if (remainingSubsets != null) {
 				long qe = calculateQe(data, subset);
 				if (first || qe < minQe) { minQe = qe; first = false; }
 
-				System.out.printf("Match: %s QE=%d %s %s\n", subsetToString(data, subset), qe, subsetToString(data, s2), subsetToString(data, s3));
-
+//				System.out.printf("Match: %s QE=%d %s\n", subsetToString(data, subset), qe,
+//					remainingSubsets.stream().map(s -> subsetToString(data, s)).collect(Collectors.joining(" "))
+//				);
 			}
 
 		}
@@ -145,7 +179,9 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 		var testData = parse(Path.of("day24/test-input"));
 		Util.assertEqual(solve1(testData), 99);
+		Util.assertEqual(solve2(testData), 44);
 		var data = parse(Path.of("day24/input"));
 		System.out.println(solve1(data));
+		System.out.println(solve2(data));
 	}
 }
